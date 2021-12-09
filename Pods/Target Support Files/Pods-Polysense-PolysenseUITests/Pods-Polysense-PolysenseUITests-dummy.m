7765f75b3422d2b5300949191867b0eb8ed6adfa
JacobTempleton <jacobaletempleton@gmail.com>
2021-12-09 11:11:54 +0600

    added CocoaPods

---
+++ Pods/Target Support Files/Pods-Polysense-PolysenseUITests/Pods-Polysense-PolysenseUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Polysense_PolysenseUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Polysense_PolysenseUITests
+@end
---
+++ Pods/Target Support Files/Pods-PolysenseTests/Pods-PolysenseTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PolysenseTests : NSObject
+@end
+@implementation PodsDummy_Pods_PolysenseTests
+@end
